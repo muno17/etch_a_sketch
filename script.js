ea6b54eca2633f35da77c197258b8a907d8a6457
muno17 <munozd17@gmail.com>
2024-04-26 10:19:36 -0400

    update createGrid function to allow varying sizes

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 // create 16x16 grid
 
+// grab button
+const button = document.querySelector("button");
+
 //grab container
 const gridContainer = document.querySelector("#gridContainer");
 
 // create 16x16 grid
-window.addEventListener("load", () => {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+window.addEventListener("load", createGrid(16));
+
+function createGrid(size) {
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.classList.add("gridSquare");
+            gridSquare.style.height = `${(600 - 2 * size) / size}px`;
+            gridSquare.style.width = `${(600 - 2 * size) / size}px`;
 
             gridContainer.appendChild(gridSquare);
         }
     }
+}
+
+button.addEventListener("click", () => {
+    let size = prompt("Enter a size");
+    createGrid(size);
 });
 
 gridContainer.addEventListener("mouseover", (e) => {
