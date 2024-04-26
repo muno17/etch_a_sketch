e56976651cf71a856abdc10cbab661ffe93beb4d
muno17 <munozd17@gmail.com>
2024-04-26 08:26:57 -0400

    add event listener to change color of grid square when moused over

--- script.js
+++ script.js
@@ -3,17 +3,20 @@
 //grab container
 const gridContainer = document.querySelector("#gridContainer");
 
-
 // create 16x16 grid
 window.addEventListener("load", () => {
-    console.log("loaded");
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.classList.add("gridSquare");
-            console.log(gridSquare);
 
             gridContainer.appendChild(gridSquare);
         }
+    }
+});
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target != gridContainer) {
+        e.target.style.backgroundColor = "yellow";
     }
 });
